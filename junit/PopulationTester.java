9353d6cf083fea33f9ff526f2af6c66b9c7ee81d
Fredrik Nystad <nystad.fredrik@gmail.com>
2015-04-12 11:53:33 +0200

    Refractor of PopulationTester.java

--- junit/PopulationTester.java
+++ junit/PopulationTester.java
@@ -1,28 +1,28 @@
 package junit;
 
 import java.io.IOException;
+
 import org.junit.Test;
+
 import doris.backend.Population;
 
 public class PopulationTester {
+	// "C:/Development/Workspace/Eclipse/Foobar/test.csv"
+	// "K:/D642-raw-data-dd.csv"
+	String filePath = "C:/Development/Workspace/Eclipse/Foobar/million.csv"; //Choose the correct path to the file
 
 	@Test
 	public void initPopulationTest() {		
 		System.gc();
 		long startTime = System.nanoTime();		
 		try {
-			Population.init("K:/D642-raw-data-dd.csv");
+			Population.init(filePath);
 		} catch (InterruptedException | IOException e) {			
 			e.printStackTrace();
 		}	
 		double endTime = (System.nanoTime() - startTime) / 1000000000.0;
 		int size = Population.getSize();
-		double averageNumATcs = Population.getAverageNumATCs();
-		double averageNumICDs = Population.getAverageNumICDs();
 		
-		System.out
-				.printf("Time: %,.2fs |Population size: %d | AverageNumOfDiseases: %,.2f | AverageNumOfDrugs: %,.2f%n%n",
-						endTime, size, averageNumATcs, averageNumICDs);
-		
+		System.out.printf("Time: %,.2fs |Population size: %d %n%n",endTime, size);		
 	}
 }
